0267f3e37b94363ed5e4841b1f844d24ee3ca586
yfm <yangfangming@tuandai.com>
2019-01-03 18:49:00 +0800

    pod

--- iGithub/iGithub/Modules/Activity/ViewController/ActivityViewController.swift
+++ iGithub/iGithub/Modules/Activity/ViewController/ActivityViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class ActivityViewController: UIViewController {
 
+    let url = "https://api.github.com/users/FMYang/events"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
